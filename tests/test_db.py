1823f018c467eea4d0d0bd32ad184c31d3a6688e
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-07-13 19:46:46 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,48 +1,39 @@
-# tests/test_app.py
+# tests/test_db.py
 
 import unittest
-import os
-os.environ['TESTING'] = 'true'
+from peewee import *
 
-from app import app
+from app import TimelinePost
 
-class AppTestCase(unittest.TestCase):
-    def setUp(self):
-        self.client = app.test_client()
+MODELS = [TimelinePost]
 
-    def test_home(self):
-        response = self.client.get("/")
-        assert response.status_code == 200
-        html = response.get_data(as_text=True)
-        assert '<title>MLH Fellow</title>' in html
-        # TODO Add more tests relating to the home page
+# use an in-memory SQLite for tests
+test_db = SqliteDatabase(':memory:')
+class TestTimelinePost(unittest.TestCase):
+	def setUp(self):
+		# Bind model classes to test db. Since we have a complete list of all models, we do not 
+		# need recursively bind dependencies.
+		test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
 
-    def test_timeline(self):
-        response = self.client.get('/api/timeline_post')
-        assert response.status_code == 200
-        assert response.is_json
-        json = response.get_json()
-        assert 'timeline_posts' in json
-        assert len(json['timeline_posts']) == 0
-        # TODO Add more tests relating to the /api/timeline_post GET and POST apis
-        # TODO Add  more tests relating to the timeline page
+		test_db.connect()
+		test_db.create_tables(MODELS)
+	
+	def tearDown(self):
+		# Not strictly necessary since SQLite in-memory databases only live for the duration of
+		# the connection, and in the next step we close the connection..but a good practice all
+		# the same
+		test_db.drop_tables(MODELS)
 
+		# Close connection to db
+		test_db.close()
 
-    def test_malformed_timeline_post(self):
-        # POST request missing name
-        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid name" in html
-
-        # POST request with empty content
-        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content" : ""})
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid content" in html
-
-        # POST request with malformed email
-        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content" : "Hello world, I'm John!"})
-        assert response.status_code == 400
-        html = response.get_data(as_text=True)
-        assert "Invalid email" in html
+	def test_timeline_post(self):
+		# Create 2 timeline posts
+		first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+		assert first_post.id == 1
+    
+		second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+		assert second_post.id == 2
+		
+		response = get_time_line_post()
+		assert len(response['timeline_posts']) == 2
