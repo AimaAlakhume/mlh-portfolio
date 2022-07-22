1e82e5eedc2cd2ea435f8d333de986ad54586f57
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2022-07-22 11:10:40 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,7 @@
 
 cd /root/mlh-portfolio
 
-git fetch && git reset --hard  origin/main
+git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-systemctl restart myportfolio.service
+docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml up -d --build
