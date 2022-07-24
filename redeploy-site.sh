3775b26d1ed8f2268c9aa97a8cafe3a034df308c
root <aimailene@gmail.com>
2022-07-24 00:19:16 +0000

    redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,5 +4,5 @@ cd /root/mlh-portfolio
 
 git fetch && git reset origin/main --hard
 
-docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
