7a6885c13cf4f44ec1ff7024a658da629075cc4f
Aima Alakhume <aimailene@gmail.com>
2022-08-01 17:28:46 -0400

    tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,18 +21,25 @@ class AppTestCase(unittest.TestCase):
         html = response.get_data(as_text=True)
         assert '<title>About Aima</title>' in html
 
-
     def test_timeline(self):
-        response = self.client.get('/api/show_posts')
-        print(response.status_code)
-        assert response.status_code >= 200
+        response = self.client.get("/api/show_posts")
+        assert response.status_code == 200
+        print(response.status_code) #debug
         assert response.is_json
         json = response.get_json()
-        assert 'timeline_posts' in json
-        assert len(json['timeline_posts']) == 0
-        # TODO Add more tests relating to the /api/timeline GET and POST apis
-        # TODO Add more tests relating to the timeline page
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
+        
+        # test /api/timeline_post GET and POST apis
+        response = self.client.post("/api/show_posts", data={"name": "john", "email": "john@example.com", "content": "Hello world, I'm John!"})
+        html = response.get_data(as_text=True)
+        assert "john" in html
+        assert "john@example.com" in html
+        assert "Hello world, I'm John!" in html
 
+        response = self.client.get("/api/show_posts")
+        json = response.get_json()
+        assert len(json["timeline_posts"]) == 1
 
     def test_malformed_timeline_post(self):
         # POST request missing name
@@ -45,7 +52,7 @@ class AppTestCase(unittest.TestCase):
         response = self.client.post("/api/timeline", data={"name": "Aima Alakhume", "email": "aimailene@gmail.com", "content" : ""})
         assert response.status_code >= 400
         html = response.get_data(as_text=True)
-        print(html)
+        print('HTML', html)
         assert "Bad Request" in html
 
         # POST request with malformed email
