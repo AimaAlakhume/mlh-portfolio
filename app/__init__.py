f6ea29572ba0c4588537e547c2a24d91eee87d58
Aima Alakhume <aimailene@gmail.com>
2022-08-01 17:50:27 -0400

    app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -98,7 +98,7 @@ def post_time_line_post():
 
     if 'email' not in request.form or validate(request.form['email']) == False:
         return Response("Invalid email", status=400)
-    if 'content' not in request.form or validate(request.form['content']) == False
+    if 'content' not in request.form or validate(request.form['content']) == False:
         return Response("Invalid content", status=400)
     
     name = request.form["name"]
